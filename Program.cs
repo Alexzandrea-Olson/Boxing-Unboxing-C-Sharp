70dbc6a2e116064be7f5bb873cc1ac75bedae14d
Alexzandrea M Olson <98174336+Alexzandrea-Olson@users.noreply.github.com>
2022-05-24 02:28:40 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables
+            int x = 12;
+            double y = 12.5;
+            String name = "Hannah";
+
+            //Boxing
+            object obj1 = x;
+            object obj2 = y;
+            object obj3 = name;
+
+            //unboxing 
+            int x1 = (Int32)obj1;
+            double y1 = (Double)obj2;
+            string x2 = (String)obj3;
+
+            Console.WriteLine(x1);
+            Console.WriteLine(y1);
+            Console.WriteLine(x2);
+        }
+    }
+}
